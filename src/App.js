ab5a6c665b1e66926a6513d4e31780edb61e1df5
Mijolae <mijolae@gmail.com>
2022-12-05 20:55:01 -0600

    feat: share functionality

--- src/App.js
+++ src/App.js
@@ -22,26 +22,6 @@ function App() {
       .catch((e) => <p>Error: {e}</p>);
   };
 
-  // fetch(
-  //   'https://th-thumbnailer.cdn-si-edu.com/hMEJcNlfYQkWAY7UWinCN7Bob_0=/1072x720/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/Surprising-Science-baby-seal-631.jpg'
-  // )
-  //   .then(function (response) {
-  //     return response.blob();
-  //   })
-  //   .then(function (blob) {
-  //     var file = new File([blob], 'picture.jpg', { type: 'image/jpeg' });
-  //     var filesArray = [file];
-
-  //     if (navigator.canShare && navigator.canShare({ files: filesArray })) {
-  //       navigator.share({
-  //         text: 'some_text',
-  //         files: filesArray,
-  //         title: 'some_title',
-  //         url: 'some_url',
-  //       });
-  //     }
-  //   });
-
   return (
     <div className='App'>
       {/* <Sharer label="Share" /> */}
